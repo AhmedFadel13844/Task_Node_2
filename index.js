44146ec6e7776f8fb35d728cc54a356daa2590dd
AhmedFadel13844 <a7medfadel13844@gmail.com>
2023-06-15 18:03:49 +0200

    updata data

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 function getProducts(products){
  let categories = [];
+
  for (let i = 0; i < products.length; i++) {
- categories.push(products[i]['category']['name']);
-}
-
-let unique_categories = [...new Set(categories)];
-let new_products = [];
-
-
-for (let i = 0; i < unique_categories.length; i++) {
- let cat_products = {"category": {}, "products": []};
- for (let j = 0; j < products.length; j++) {
-   if (products[j]['category']['name'] == unique_categories[i]) {
-     cat_products['category'] = unique_categories[i];
-     cat_products['products'].push(products[j]);
-   }
+   let category = [];
+   category.push(products[i]['category']['name']);
+   category.push(products[i]['category']['id']);
+   categories.push(category);
  }
- new_products.push(cat_products);
-}
-console.log(new_products);
+ 
+ let unique_categories = [...new Set(categories.map(JSON.stringify))].map(JSON.parse);
+ let new_products = [];
+ 
+ let cat_products = {"category": {}, "products": []};
+ 
+ for (let i = 0; i < unique_categories.length; i++) {
+   cat_products = {"category": {}, "products": []};
+   for (let j = 0; j < products.length; j++) {
+     if (products[j]['category']['name'] == unique_categories[i][0]) {
+       cat_products['category']['name'] = unique_categories[i][0];
+       cat_products['category']['id'] = unique_categories[i][1];
+       cat_products['products'].push(products[j]);
+     }
+   }
+   new_products.push(cat_products);
+ }
+ console.log(new_products);
+ let fs = require("fs");
+
+fs.writeFile('my_data.json', JSON.stringify(new_products), err => {
+ if (err) {
+   console.error(err);
+   return;
+ }
+})
+
 }
 
  async function logJSONData() {
@@ -29,3 +44,4 @@ console.log(new_products);
 }
 
 logJSONData();
+
